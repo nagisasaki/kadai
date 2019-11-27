c161d7cdadadeeae5aea0e5707c44f53a2cb4095
nagisasaki <h.ca.zansoe95@gmail.com>
2019-11-27 05:24:21 +0000

    課題を追加

--- kadai6/index-2.js
+++ kadai6/index-2.js
@@ -5,38 +5,41 @@ const str4 = '{"a": undefined}'
 const str5 = '{"a": 123}'
 const str6 = '{a: 123}'
 
+// str1 のパース結果
 try {
     console.log(JSON.parse(str1))
 } catch (error) {
     console.log(error)
+    // abcという文字例は正しい JSONではないから。
+    // 正しくは　"abc"
 }
 
 try {
-    console.log(JSON.paese(str2))
+    console.log(JSON.parse(str2))
 } catch (error) {
     console.log(error)
 }
 
 try {
-    console.log(JSON.paese(str3))
+    console.log(JSON.parse(str3))
 } catch (error) {
     console.log(error)
 }
 
 try {
-    console.log(JSON.paese(str4))
+    console.log(JSON.parse(str4))
 } catch (error) {
     console.log(error)
 }
 
 try {
-    console.log(JSON.paese(str5))
+    console.log(JSON.parse(str5))
 } catch (error) {
     console.log(error)
 }
 
 try {
-    console.log(JSON.paese(str6))
+    console.log(JSON.parse(str6))
 } catch (error) {
     console.log(error)
 }
